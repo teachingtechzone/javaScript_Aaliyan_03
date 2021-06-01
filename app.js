419a06f5184ff42f5f3c5ba2e3bd6b19eeef67a4
hasnain <teachingtechzone@gmail.com>
2021-06-01 05:05:12 -0700

    class 1st ended with expression

---
+++ app.js
@@ -0,0 +1,39 @@
+// console.log("yes");
+
+
+// alert("yes")
+// window.alert("yes")
+
+
+// document.write("yes")
+
+// console.log(window)
+
+
+
+// var 
+
+// var firstname = "Aaliyan";
+// var lastname = "iqbal";
+
+
+// var myname1 = firstname + lastname; // concatination
+
+
+// // alert(myname);
+
+// console.log(myname1);
+
+
+
+// var myname2 = firstname + " " + lastname; // concatination
+
+// console.log(myname2);
+
+
+// // expression 
+// console.log(2+5);
+// console.log(2+"5");
+
+
+
